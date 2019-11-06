0cec0a436840ce77f7d408fd83f2102e0b3d3aa1
Mauricio Degregori <degregori2@gmail.com>
2019-11-05 19:19:27 -0600

    app created

---
+++ reducer-todo/src/reducers/todoReducer.js
@@ -0,0 +1,14 @@
+export const initialState = [
+    {
+        item: 'Learn about reducers',
+        completed: false,
+        id: Date.now()
+      }
+]
+
+export const reducer =  (state, action) => {
+    switch(action.type){
+        default: 
+        return state; 
+    }
+}
